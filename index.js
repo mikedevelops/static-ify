b088e3d829cc880e39232c9b009ba7bb3b775626
Mike Smart <mike.smart@jadu.co.uk>
2017-08-31 14:07:29 +0100

    split resources into local, downloadable, replaced & ignored for (hopefully) more readable code and better logging, fixed protocol relative URLs with url.resolve

--- index.js
+++ index.js
@@ -51,7 +51,10 @@ global.logger.level = 'info';
 (async function () {
     await staticify.start();
 
-    global.logger.log('info', `Got ${resourceManager.getSuccessfulResources().length} resources successfully`)
-    global.logger.log('info', `Failed to get ${resourceManager.getFailedResources().length} resources`)
-    global.logger.log('info', `Logged ${errorService.getErrors().length} errors`)
+    global.logger.log('info', '**************************************');
+    global.logger.log('info', `Got ${resourceManager.getSuccessfulResources().length} resources successfully`);
+    global.logger.log('info', `Replaced ${resourceManager.getReplaced().length} resource URLs`);
+    global.logger.log('info', `Ignored ${resourceManager.getIgnored().length} resource URLs`);
+    global.logger.log('info', `Failed to get ${resourceManager.getFailedResources().length} resources`);
+    global.logger.log('info', `Logged ${errorService.getErrors().length} errors`);
 })();
